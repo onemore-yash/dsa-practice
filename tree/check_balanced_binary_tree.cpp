a22a8a132cd15f54e313e6ab69108e47d0e880bb
Yash Kumbhani <yashkumbhani2007@gmail.com>
2026-06-23 19:34:29 +0530

    Add Solution of Balanced Binary Tree

---
+++ tree/check_balanced_binary_tree.cpp
@@ -0,0 +1,39 @@
+// Problem: Balanced Binary Tree
+// Link: https://leetcode.com/problems/balanced-binary-tree/description/
+// Approach: check for every node left and right height if diff>1 then return -1
+// Date Solved: 23-06-2026
+
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+    int height(TreeNode *node)
+    {
+        if (!node)
+            return 0;
+        int lh = height(node->left);
+        if (lh == -1)
+            return -1;
+        int rh = height(node->right);
+        if (rh == -1)
+            return -1;
+        if (abs(rh - lh) > 1)
+            return -1;
+        return 1 + max(lh, rh);
+    }
+
+public:
+    bool isBalanced(TreeNode *root)
+    {
+        return (height(root) != -1);
+    }
+};
