82b4531e2065be75c61976bf8c1ce6238f3cbeaa
Yash Kumbhani <yashkumbhani2007@gmail.com>
2026-06-24 21:59:44 +0530

    Add Solution of Check balanced BT

--- trees/balance_a_bst.cpp
+++ trees/balance_a_bst.cpp
@@ -18,26 +18,3 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-class Solution
-{
-    int height(TreeNode *node)
-    {
-        if (!node)
-            return 0;
-        int lh = height(node->left);
-        if (lh == -1)
-            return -1;
-        int rh = height(node->right);
-        if (rh == -1)
-            return -1;
-        if (abs(rh - lh) > 1)
-            return -1;
-        return 1 + max(lh, rh);
-    }
-
-public:
-    bool isBalanced(TreeNode *root)
-    {
-        return (height(root) != -1);
-    }
-};
--- trees/check_balanced_bt.cpp
+++ trees/check_balanced_bt.cpp
@@ -20,5 +20,24 @@ struct TreeNode
 };
 class Solution
 {
+    int height(TreeNode *node)
+    {
+        if (!node)
+            return 0;
+        int lh = height(node->left);
+        if (lh == -1)
+            return -1;
+        int rh = height(node->right);
+        if (rh == -1)
+            return -1;
+        if (abs(rh - lh) > 1)
+            return -1;
+        return 1 + max(lh, rh);
+    }
+
 public:
+    bool isBalanced(TreeNode *root)
+    {
+        return (height(root) != -1);
+    }
 };
