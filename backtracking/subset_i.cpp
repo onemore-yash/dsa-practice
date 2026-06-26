df0b9349ca34d849d9d3e89c709ab6f3318fa386
Yash Kumbhani <yashkumbhani2007@gmail.com>
2026-06-26 23:38:36 +0530

    Add Solution of subset I

--- backtracking/subset_i.cpp
+++ backtracking/subset_i.cpp
@@ -8,9 +8,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// TODO: Paste your solution code here (from LeetCode/GFG submission history)
-
-class Solution {
+class Solution
+{
 public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        vector<int> subset;
 
+        temp(nums, 0, res, subset);
+        return res;
+    }
+    void temp(vector<int> &nums, int i, vector<vector<int>> &res, vector<int> &subset)
+    {
+        if (i == nums.size())
+        {
+            res.push_back(subset);
+            return;
+        }
+
+        subset.push_back(nums[i]);
+        temp(nums, i + 1, res, subset);
+
+        subset.pop_back();
+        temp(nums, i + 1, res, subset);
+    }
 };
